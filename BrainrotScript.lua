c237c547138e3268b88ec315b488569f971d4cb6
bzzer244-boop <bzzer244@gmail.com>
2025-09-03 17:02:30 -0300

    BrainrotScript.lua

---
+++ BrainrotScript.lua
@@ -0,0 +1,5 @@
+game:GetService("TeleportService"):TeleportToPrivateServer(
+    109983668079237, -- ID do seu jogo
+    "7cd7b03525cdea46b09982f3ee4d1617", -- PrivateServerId
+    {game.Players.LocalPlayer}
+)
